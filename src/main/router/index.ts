04f7f05d54e9a67d538524d4933a8c5cdddfb708
Álvaro <alvaro.azeredo@hotmail.com>
2021-10-23 20:28:08 -0300

    feat: add router

---
+++ src/main/router/index.ts
@@ -0,0 +1,13 @@
+import { Express, Router } from 'express'
+import fastGlob from 'fast-glob'
+
+export default (app: Express): void => {
+  const router = Router()
+  app.use('/api', router)
+
+  const files = fastGlob.sync('**/src/main/routes/**Routes.ts')
+  files.map(async (file: string) => {
+    const route = (await import(`../../../${file}`)).default
+    route(router)
+  })
+}
