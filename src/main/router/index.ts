3fe70ebdb1a8348a49f65a6b8260b09b505d715c
Álvaro <alvaro.azeredo@hotmail.com>
2021-10-23 21:03:09 -0300

    fix: fix path of routes in router

--- src/main/router/index.ts
+++ src/main/router/index.ts
@@ -5,7 +5,7 @@ export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
 
-  const files = fastGlob.sync('**/src/main/router/routes/**.route.ts')
+  const files = fastGlob.sync('**/src/main/router/routes/**.routes.ts')
   files.map(async (file: string) => {
     const route = (await import(`../../../${file}`)).default
     route(router)
